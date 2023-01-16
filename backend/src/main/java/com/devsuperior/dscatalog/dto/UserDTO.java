52d51d0cdf949ee421f819e56af2b67f56604c4c
Pedro Henrique Azevedo <phazevedo28@gmail.com>
2023-01-16 20:34:55 -0300

    "Basic Beans Validation"

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
@@ -4,14 +4,21 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import com.devsuperior.dscatalog.entities.User;
 
 public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@NotBlank(message = "Campo Obrigatório")
 	private String firstName;
 	private String lastName;
+	
+	@Email(message = "Favor digitar email válido")
 	private String email;
 
 	private Set<RoleDTO> roles = new HashSet<>();
@@ -22,7 +29,7 @@ public class UserDTO implements Serializable {
 
 	public UserDTO(Long id, String firstName, String lastName, String email, String password) {
 		this.id = id;
-		this.firstName = firstName;
+ 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 	}
--- backend/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
@@ -2,6 +2,8 @@ package com.devsuperior.dscatalog.resources;
 
 import java.net.URI;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -57,14 +59,14 @@ public class UserResource {
 	}
 
 	@PostMapping
-	public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO insertDto) {
+	public ResponseEntity<UserDTO> insert( @Valid @RequestBody UserInsertDTO insertDto) {
 		UserDTO dto  = service.insert(insertDto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
 	}
 
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody UserDTO dto) {
+	public ResponseEntity<UserDTO> update(@PathVariable Long id,@Valid @RequestBody UserDTO dto) {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
